ede8d38ae57dd15a6a6777b2c9ba211b90b29520
Apoorva Singh <67651664+ApoorvaSingh939@users.noreply.github.com>
2022-06-09 00:10:12 +0530

    Add files via upload

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+import {getProductById, getProducts} from '../controllers/productController.js';
+
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
+
+export default router;
